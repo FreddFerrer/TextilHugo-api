0e1bb391fabe9752f92e0d8ba08843ea8fb22dbd
Freddy <ferreirafreddy39@gmail.com>
2024-01-18 19:36:56 -0300

    first commit

---
+++ src/main/java/com/fredd/TextilHugo_web/TextilHugoWebApplication.java
@@ -0,0 +1,13 @@
+package com.fredd.TextilHugo_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextilHugoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextilHugoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fredd/TextilHugo_web/TextilHugoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredd.TextilHugo_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextilHugoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
